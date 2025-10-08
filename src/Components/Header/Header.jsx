36c42cb142024ac0426b631ae6fa22b2cb0a712c
imadisimran <imadisimran@gmail.com>
2025-10-08 11:30:53 +0600

    header added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router';
+import logo from '../../assets/logo.png'
+import './Header.css'
+import { Github } from 'lucide-react';
+
+const Header = () => {
+
+    const navLinks = <>
+        <NavLink to='/'><li>Home</li></NavLink>
+        <NavLink to='/apps'><li>Apps</li></NavLink>
+        <NavLink to='/installation'><li>Installation</li></NavLink>
+    </>
+
+
+    return (
+        <header className='bg-base-100 shadow-sm'>
+            <div className="navbar w-11/12 mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow font-bold">
+                            {navLinks}
+                        </ul>
+                    </div>
+                    {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
+                    <Link to='/'>
+                    <div className='flex gap-3 items-center'>
+                        <img className='max-w-15' src={logo} alt='Logo' />
+                        <h3 className='font-bold text-lg text-[#6e38e6]'>HERO.IO</h3>
+                    </div>
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 gap-8 font-bold">
+                        {navLinks}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a href='' className="btn bg-gradient-to-br from-[#6e38e6] to-[#9d70ff] text-white"><Github />Contribute</a>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Root/Root";
+import Home from "../Pages/Home/Home";
+import Apps from "../Pages/Apps/Apps";
+import Installation from "../Pages/Installation/Installation";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Root,
+    children:[
+        {
+            index:true,
+            Component:Home
+        },
+        {
+            path:'apps',
+            Component:Apps
+        },
+        {
+            path:'installation',
+            Component:Installation
+        }
+    ]
+  },
+]);
